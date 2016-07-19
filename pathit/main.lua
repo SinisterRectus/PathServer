2b8d259121ff8c9da7ff8d6cbc808cde57cae1a5
SinisterRectus <sinister.rectus@gmail.com>
2016-07-18 20:41:30 -0400

    Removed a bunch of debug code

--- pathit/main.lua
+++ pathit/main.lua
@@ -1,16 +1,5 @@
 local Graph = require('./classes/Graph')
-local Node = require('./structs/Node')
 local config = require('./config')
-local uv = require('uv')
-local ffi = require('ffi')
-
-function table.count(tbl)
-	local n = 0
-	for k in pairs(tbl) do
-		n = n + 1
-	end
-	return n
-end
 
 local function getMemoryUsage()
 	collectgarbage()
@@ -21,20 +10,8 @@ end
 local m = getMemoryUsage()
 local t = os.clock()
 
-local n = 10000
-
-local size = config.cellSize / config.nodeSize
-
-local cells = {}
-for x = 0, 63 do
-	for y = 0, 63 do
-		local cell = {}
-		cell.nodes = ffi.new(string.format('Node[%i][%i]', size, size))
-		table.insert(cells, cell)
-	end
-end
-
--- local graph = Graph()
--- graph:loadCell(6, 29)
+local graph = Graph()
+graph:loadCell(6, 29)
+graph:startServer(config.host, config.port)
 
 p(getMemoryUsage() - m, os.clock() - t)
